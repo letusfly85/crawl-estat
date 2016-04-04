6ab9815f2809efa958fc5f7dbd8bdf7723b65a4a
letusfly85 <letusfly95@gmail.com>
2016-04-04 11:54:14 +0900

    parse items

--- test/crawl_estat_test.exs
+++ test/crawl_estat_test.exs
@@ -3,6 +3,15 @@ defmodule CrawlEstatTest do
   doctest CrawlEstat
 
   test "the truth" do
+    #CrawlEstat.find_life_table
+    ids = CrawlEstat.find_stats_list("00450012", "2016")
+
+    IO.inspect(ids)
+
+    Enum.each(ids, fn(id) ->
+      CrawlEstat.find_life_table(id)
+    end)
+
     assert 1 + 1 == 2
   end
 end
